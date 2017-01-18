efb5412419a748318b715e37de683f80d8900258
Derek Gifford <derekisbusy@gmail.com>
2017-01-17 21:56:13 -0500

    Add contact migration.

---
+++ migrations/m170110_000101_create_contact_reason_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use yii\db\Schema;
+
+class m170110_000101_create_contact_reason_table extends \yii\db\Migration
+{
+    public function up()
+    {
+        $tables = Yii::$app->db->schema->getTableNames();
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        
+        if (!in_array(Yii::$app->db->tablePrefix.'contact_reason', $tables))  { 
+            $this->createTable('{{%contact_reason}}', [
+                'id' => $this->primaryKey(),
+                'created_at' => $this->datetime(),
+                'updated_at' => $this->datetime(),
+                'created_by' => $this->integer(11),
+                'updated_by' => $this->integer(11),
+                'reason' => $this->string(255),
+            ], $tableOptions);
+
+
+            // Created By
+            $this->createIndex(
+                'idx-contact_reason-created_by',
+                '{{%contact_reason}}',
+                'created_by'
+            );
+
+            $this->addForeignKey(
+                'fk-contact_reason-created_by',
+                '{{contact_reason}}',
+                'created_by',
+                '{{%user}}',
+                'id',
+                'SET NULL',
+                'CASCADE'
+            );
+
+            // Updated By
+            $this->createIndex(
+                'idx-contact_reason-updated_by',
+                '{{%contact_reason}}',
+                'updated_by'
+            );
+
+            $this->addForeignKey(
+                'fk-contact_reason-updated_by',
+                '{{contact_reason}}',
+                'updated_by',
+                '{{%user}}',
+                'id',
+                'SET NULL',
+                'CASCADE'
+            );
+        }
+    }
+
+    public function down()
+    {
+        $this->execute('SET foreign_key_checks = 0');
+        $this->dropTable('{{%contact_reason}}');
+        $this->execute('SET foreign_key_checks = 1;');
+    }
+}
