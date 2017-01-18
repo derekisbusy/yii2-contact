5ff1587b1e78a4830e75d71ac8c4449d41ab4859
Derek Gifford <derekisbusy@gmail.com>
2017-01-17 21:55:58 -0500

    Add contact_notify migration.

---
+++ migrations/m170110_000101_create_contact_notify_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Schema;
+
+class m170110_000101_create_contact_notify_table extends \yii\db\Migration
+{
+    public function up()
+    {
+        $tables = Yii::$app->db->schema->getTableNames();
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        
+        if (!in_array(Yii::$app->db->tablePrefix.'contact_notify', $tables))  { 
+            $this->createTable('{{%contact_notify}}', [
+                'id' => $this->primaryKey(),
+                'email' => $this->string(255)->notNull(),
+            ], $tableOptions);
+        }
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%contact_notify}}');
+    }
+}
