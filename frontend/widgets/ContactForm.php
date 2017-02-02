13c3be2134c8cd62472cb1987f4de16ed7701fb8
Derek Gifford <derekisbusy@gmail.com>
2017-02-02 02:26:50 -0500

    Add message and default reason to ContactForm widget.

--- frontend/widgets/ContactForm.php
+++ frontend/widgets/ContactForm.php
@@ -9,16 +9,27 @@ class ContactForm extends \yii\base\Widget
 {
     public $model;
     
+    /**
+     * Display textarea for message input.
+     * @var boolean
+     */
+    public $message = true;
+    
+    public $defaultReason;
+    
     public function init()
     {
         parent::init();
         if (!$this->model) {
             $this->model = new Contact;
         }
+        if ($this->defaultReason) {
+            $this->model->contact_reason_id = $this->defaultReason;
+        }
     }
     
     public function run()
     {
-        return $this->render('_form', ['model' => $this->model]);
+        return $this->render('_form', ['model' => $this->model, 'message' => $this->message]);
     }
 }
