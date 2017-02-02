2fa23dbf86a2f4f31862352cea2e42f3a291199d
Derek Gifford <derekisbusy@gmail.com>
2017-02-02 02:25:41 -0500

    Update actionForm() method.

--- frontend/modules/contact/controllers/RequestController.php
+++ frontend/modules/contact/controllers/RequestController.php
@@ -2,7 +2,9 @@
 
 namespace derekisbusy\contact\frontend\modules\contact\controllers;
 
+use derekisbusy\contact\models\base\ContactNotifyReason;
 use derekisbusy\contact\models\Contact;
+use derekisbusy\contact\models\ContactNotify;
 use Yii;
 use yii\web\Controller;
 
@@ -21,9 +23,41 @@ class RequestController extends Controller
     {
         
         $model = new Contact();
-
+        
+        $model->url = $_SERVER['REQUEST_URI'];
+        $model->referrer = $_SERVER['HTTP_REFERER'];
+        
         if ($model->loadAll(Yii::$app->request->post()) && $model->saveAll()) {
-            return $this->render(['success']);
+            
+            if (Yii::$app->params['adminEmail']) {
+                $result = Yii::$app->mailer->compose()
+                ->setTo(Yii::$app->params['adminEmail'])
+                ->setFrom([$model->email => $model->name])
+                ->setSubject('Contact: '.$model->reason->reason)
+                ->setTextBody($model->body)
+                ->send();
+            }
+            
+            $rows = (new \yii\db\Query())
+                ->select(['email'])
+                ->from(ContactNotify::tableName().' n')
+                ->innerJoin(ContactNotifyReason::tableName().' r', 'r.contact_notify_id = n.id')
+                ->where('r.contact_reason_id = :reason', [':reason' => $model->reason->id])
+                ->all();
+            
+            
+            foreach ($rows as $row) {
+                $result = Yii::$app->mailer->compose()
+                    ->setTo($row['email'])
+                    ->setFrom([$model->email => $model->name])
+                    ->setSubject('Contact: '.$model->reason->reason)
+                    ->setTextBody($model->body)
+                    ->send();
+            }
+            
+            Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+            
+            return $this->refresh();
         } else {
             return $this->render('form', [
                 'model' => $model,
