2e069abac259d699bf463205c20a4c382f016d31
Derek Gifford <derekisbusy@gmail.com>
2017-02-02 02:26:15 -0500

    Update contact form.

--- frontend/widgets/views/_form.php
+++ frontend/widgets/views/_form.php
@@ -1,17 +1,20 @@
 <?php
 
+use derekisbusy\contact\models\Contact;
+use derekisbusy\contact\models\ContactReason;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
-/* @var $model derekisbusy\contact\models\Contact */
+/* @var $model Contact */
 /* @var $form yii\widgets\ActiveForm */
 
 ?>
 
 <div class="contact-form">
     
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['action' => ['/contact']]); ?>
     <div class="row">
     <?= $form->errorSummary($model); ?>
     </div>
@@ -20,7 +23,11 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'name')->textInput(['maxlength' => true, 'placeholder' => 'Name']) ?>
         </div>
         <div class="col-md-6">
-    <?= $form->field($model, 'contact_reason_id')->textInput(['placeholder' => 'Contact Reason']) ?>
+    <?= $form->field($model, 'contact_reason_id')
+        ->dropDownList(
+            ArrayHelper::map(ContactReason::find()->all(), 'id', 'reason'),           // Flat array ('id'=>'label')
+            ['prompt'=>'']    // options
+        ); ?>
         </div>
     </div>
     <div class="row">
@@ -31,11 +38,14 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'phone')->textInput(['maxlength' => true, 'placeholder' => 'Phone']) ?>
         </div>
     </div>
+    <?php if ($message) : ?>
     <div class="row">
         <div class="col-md-12">
     <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
         </div>
     </div>
+    <?php endif; ?>
+    
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Submit') , ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
